a79a28004c4f7312e73cac04b47ca759dff42800
TREY <hugh.a.lloyd@gmail.com>
2020-01-22 17:39:10 -0600

    beginning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+  let [apodData] = useState([])
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=6blFCQncdXvLUKRwdZYiDB7hUDnzf67dy6DvF7l0').then
+    (response => {
+      console.log(response)
+    }).catch(error => {
+      console.log('ERROR', error)
+    })
+  },[]);
   return (
+ 
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
